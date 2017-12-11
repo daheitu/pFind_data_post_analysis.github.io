4cfcb9e3483c3023d776a3128e1fd57d36d7b904
daheitu <zhiyong.cao@outlook.com>
2017-12-11 09:36:59 +0800

    gguj

--- filter_mgf.py
+++ filter_mgf.py
@@ -1,22 +1,26 @@
 import os
+
+
 def generate_ion_mass_range(num):
-    deta = num * 0.5/ 10000
+    deta = num * 0.5 / 10000
     return num - deta, num + deta
 
+
 def words_frequcecy(list):
-    mass_frequcy_dic={}
+    mass_frequcy_dic = {}
     for word in list:
         if word not in mass_frequcy_dic:
-            mass_frequcy_dic[word]=0
+            mass_frequcy_dic[word] = 0
         mass_frequcy_dic[word] += 1
-     #x,y : operator.lt(x[1],y[1]), reverse=True)
-    return sorted(mass_frequcy_dic.items(), key=lambda d:d[1], reverse=True)
+    #x,y : operator.lt(x[1],y[1]), reverse=True)
+    return sorted(mass_frequcy_dic.items(), key=lambda d: d[1], reverse=True)
+
 
 os.chdir(r"E:\TEST_AB+N\B_N")
-file_list=os.listdir(os.getcwd())
+file_list = os.listdir(os.getcwd())
 for name in file_list:
     if name[-3:] == "mgf":
-        f=open(name)
+        f = open(name)
         all = f.readlines()
         ion_be = []
         ion_end = []
@@ -29,15 +33,16 @@ for name in file_list:
         if len(ion_be) != len(ion_end):
             print("erro")
         else:
-            report_file_name=name[:name.find(".mgf")]+".txt"
+            report_file_name = name[:name.find(".mgf")] + ".txt"
             b = open(report_file_name, 'w')
-            b.write("\t".join(["title","charge","m/z","mass"]))
+            b.write("\t".join(["title", "charge", "m/z", "mass"]))
             b.write("\n")
             for k in ion_be:
                 title = all[k + 1].strip()
                 charge = all[k + 2][7:9]
-                mass_over_z = all[k + 4][all[k + 4].find("=") + 1:all[k + 4].find("\n")]
-                mass = float(mass_over_z) * int(charge[0])-int(charge[0])
+                mass_over_z = all[k + 4][all[k + 4].find("=") + 1:all[k + 4]
+                                         .find("\n")]
+                mass = float(mass_over_z) * int(charge[0]) - int(charge[0])
                 w_list = [title, charge, mass_over_z, str(mass)]
                 ms2_ion_list = []
                 #print k+5,ion_end[ion_be.index(k)]
@@ -48,7 +53,7 @@ for name in file_list:
                 [y1_low, y1_up] = generate_ion_mass_range(147.1128)
                 [y2_low, y2_up] = generate_ion_mass_range(244.1656)
                 [y3_low, y3_up] = generate_ion_mass_range(391.2340)
-                [b2_low, b2_up]= generate_ion_mass_range(201.1234)
+                [b2_low, b2_up] = generate_ion_mass_range(201.1234)
                 include_y1 = False
                 include_y2 = False
                 include_y3 = False
@@ -63,7 +68,7 @@ for name in file_list:
                         include_y2 = True
                     elif float(ion) > y3_low and float(ion) < y3_up:
                         include_y3 = True
-                if include_y1 and include_y2 and include_y3 :
+                if include_y1 and include_y2 and include_y3:
                     b.write("\t".join(w_list))
                     b.write("\n")
             b.close()
@@ -82,4 +87,3 @@ for name in file_list:
             b.write("\n")
         b.close()
         f.close()
-
--- mutiple_raw_splite_v2_for_e_value.py
+++ mutiple_raw_splite_v2_for_e_value.py
@@ -1,110 +1,165 @@
 import os
-os.chdir(r"D:\softwareData\plink2\search_task_2017.11.30.10.02.14_ARGO_CNGP\reports")
+os.chdir(
+    r"D:\softwareData\plink2\20160703\search_task_2016.07.05.15.18.33_Aldo_3\reports"
+)
 
 
+# 对list去重复
 def reduce_redundancy_list(list):
-    for word in all_site:
+    new_site_list = []
+    for word in list:
         if word not in new_site_list:
-            new_site_list.append(word)       
+            new_site_list.append(word)
     return new_site_list
 
 
-def get_crosslink_site_info():
+
+# 找到包含site信息的行
+def get_site_lines(filename):
+    site_table = open(filename).readlines()
+    ordr_pos = []
+    for i in range(len(site_table)):
+        if len([
+                x for x in site_table[i].strip().split(',') if x != ''
+        ]) == 4:
+            ordr_pos.append(i)
+    ordr_pos.append(len(site_table))
+    return ordr_pos, site_table
+
+
+# 去掉sameset
+def remove_sameset():
     filename = os.listdir(os.getcwd())
     for fl in filename:
         if fl[-22:-4] == "cross-linked_sites":
-            f = open(fl)
-            b = open("report.txt", 'w')
-            site_table = f.readlines()
-            ordr_pos = []
-            sit_list = []
-            Peptide = []
-            for i in range(2, len(site_table)):
-                if len([x for x in site_table[i].strip().split(',') if x != '']) == 4:
-                    ordr_pos.append(i)
-            ordr_pos.append(len(site_table))
-            raw_name_list = []
-            for m in range(len(ordr_pos) - 1):
-                for k in range(ordr_pos[m] + 1, ordr_pos[m + 1]):
-                    a = site_table[k].strip().split(',')[2].find(".")
-                    raw_name_list.append(site_table[k].strip().split(',')[2][:a])
-                    raw_name_list = list(set(raw_name_list))
-            raw_name_list.sort()
-            print(raw_name_list)
-            col = [
-                "Linked_site", "Total_spec", "Best_svm", "Peptide", "Peptide_mass",
-                "Prote_type"
-            ]
-            for i in range(len(raw_name_list)):
-                col.append(str(raw_name_list[i]) + "_spec")
-                col.append(str(raw_name_list[i]) + "_E_value")
-                col.append(str(raw_name_list[i]) + "unique_pep_num")
+            ordr_pos, site_table = get_site_lines(fl)
+    tmp = open("tmpfile", "w")
+    for i in range(1,len(ordr_pos) - 1):
+        Con_sameset = False
+        list_sameset = []
+        list_sameset.append(site_table[ordr_pos[i]].rstrip("\n").split(",")[1])
+        for j in range(ordr_pos[i], ordr_pos[i + 1]):
+            first_cell = site_table[j].rstrip("\n").split(",")[0]
+            if first_cell == "Sameset":
+                Con_sameset = True
+        if Con_sameset == False:
+            for j in range(ordr_pos[i], ordr_pos[i + 1]):
+                tmp.write(site_table[j])
+        else:
+            for j in range(ordr_pos[i], ordr_pos[i + 1]):
+                first_cell = site_table[j].rstrip("\n").split(",")[0]
+                if first_cell == "Sameset":
+                    list_sameset.append(
+                        site_table[j].rstrip("\n").split(",")[1])
+                elif first_cell == "":
+                    break
+            link_site = "/".join(list_sameset)
+            site_info_line_list = site_table[ordr_pos[i]].rstrip("\n").split(
+                ",")
+            site_info_line_list[1] = link_site
+            tmp.write(",".join(site_info_line_list))
+            tmp.write("\n")
+            for j in range(ordr_pos[i] + 1, ordr_pos[i + 1]):
+                first_cell = site_table[j].rstrip("\n").split(",")[0]
+                if first_cell != "Sameset":
+                    tmp.write(site_table[j])
+    tmp.close()
+    return
 
-            b.write('\t'.join(col))
-            b.write('\n')
-            m = 0
-            link_site_total_dic = {}
-            for l in range(len(ordr_pos) - 1):
-                linked_site = site_table[ordr_pos[l]].strip().split(',')[1]
-                link_site_total_dic[linked_site] = [
-                    site_table[ordr_pos[l]].strip().split(',')[1],
-                    site_table[ordr_pos[l]].strip().split(',')[3],
-                    site_table[ordr_pos[l] + 1].strip().split(',')[9],
-                    site_table[ordr_pos[l] + 1].strip().split(',')[5],
-                    site_table[ordr_pos[l] + 1].strip().split(',')[4]
-                ]
-                m = linked_site.find("(")
-                n = linked_site.find(")")
-                p = linked_site.find("-")
-                x = linked_site.find("(", p)
-                y = linked_site.find(")", p) 
-                if linked_site[:m] == linked_site[p + 1:x]:
-                    link_site_total_dic[linked_site].append("intra")
-                else:
-                    link_site_total_dic[linked_site].append("inter")
-                raw_sub_spectra_dic = {}
-                raw_sub_E_value_dic = {}
-                raw_sub_peptide_dic = {}
-                for raw in raw_name_list:
-                    raw_sub_spectra_dic[raw] = 0
-                    raw_sub_E_value_dic[raw] = []
-                    raw_sub_peptide_dic[raw] = []
-                    for j in range(ordr_pos[l] + 1, ordr_pos[l + 1]):
-                        if raw == site_table[j].strip().split(',')[2][:site_table[
-                                j].strip().split(',')[2].find(".")]:
-                            raw_sub_spectra_dic[raw] += 1
-                            raw_sub_E_value_dic[raw].append(
-                                float(site_table[j].strip().split(',')[8]))
-                            raw_sub_peptide_dic[raw].append(
-                                site_table[j].strip().split(',')[5])
-                    if raw_sub_spectra_dic[raw] == 0:
-                        link_site_total_dic[linked_site].append("")
-                        link_site_total_dic[linked_site].append("")
-                        link_site_total_dic[linked_site].append("")
-                    else:
-                        link_site_total_dic[linked_site].append(
-                            str(raw_sub_spectra_dic[raw]))
-                        link_site_total_dic[linked_site].append(
-                            str(min(raw_sub_E_value_dic[raw])))
-                        link_site_total_dic[linked_site].append(
-                            str(len(list(set(raw_sub_peptide_dic[raw])))))
-                #print(link_site_total_dic[linked_site])
-                b.write('\t'.join(link_site_total_dic[linked_site]))
-                b.write('\n')
-            b.close()
+
+def get_crosslink_site_info():
+    ordr_pos, site_table_new = get_site_lines("tmpfile")
+    b = open("report.txt", 'w')
+    sit_list = []
+    raw_name_list = []
+    for m in range(len(ordr_pos) - 1):
+        for k in range(ordr_pos[m] + 1, ordr_pos[m + 1]):
+            a = site_table_new[k].strip().split(',')[2].find(".")
+            raw_name_list.append(site_table_new[k].strip().split(',')[2][:a])
+            raw_name_list = reduce_redundancy_list(raw_name_list)
+    raw_name_list.sort()
+    print(raw_name_list)
+    col = [
+        "Linked_site", "Total_spec", "Best_e_value", "Best_svm", "Peptide",
+        "Peptide_mass", "Prote_type"
+    ]
+    for name in raw_name_list:
+        col.append(name + "_spec")
+        col.append(name + "_E_value")
+        col.append(name + "unique_pep_num")
+
+    b.write(','.join(col))
+    b.write('\n')
+    m = 0
+    link_site_total_dic = {}
+    for l in range(len(ordr_pos) - 1):
+        linked_site = site_table_new[ordr_pos[l]].strip().split(',')[1]
+        E_value_list = []
+        for i in range(ordr_pos[l] + 1, ordr_pos[l + 1]):
+            E_value_list.append(float(site_table_new[i].strip().split(',')[8]))
+        E_value_list = reduce_redundancy_list(E_value_list)
+        if "0" in E_value_list:
+            E_value_list.remove(0)
+        Best_e_value = min(E_value_list)
+        link_site_total_dic[linked_site] = [
+            site_table_new[ordr_pos[l]].strip().split(',')[1],
+            site_table_new[ordr_pos[l]].strip().split(',')[3],
+            str(Best_e_value),
+            site_table_new[ordr_pos[l] + 1].strip().split(',')[9],
+            site_table_new[ordr_pos[l] + 1].strip().split(',')[5],
+            site_table_new[ordr_pos[l] + 1].strip().split(',')[4]
+        ]
+        m = linked_site.find("(")
+        n = linked_site.find(")")
+        p = linked_site.find("-")
+        x = linked_site.find("(", p)
+        y = linked_site.find(")", p)
+        if linked_site[:m] == linked_site[p + 1:x]:
+            link_site_total_dic[linked_site].append("intra")
+        else:
+            link_site_total_dic[linked_site].append("inter")
+        raw_sub_spectra_dic = {}
+        raw_sub_E_value_dic = {}
+        raw_sub_peptide_dic = {}
+        for raw in raw_name_list:
+            raw_sub_spectra_dic[raw] = 0
+            raw_sub_E_value_dic[raw] = []
+            raw_sub_peptide_dic[raw] = []
+            for j in range(ordr_pos[l] + 1, ordr_pos[l + 1]):
+                if raw == site_table_new[j].strip().split(',')[
+                        2][:site_table_new[j].strip().split(',')[2].find(".")]:
+                    raw_sub_spectra_dic[raw] += 1
+                    raw_sub_E_value_dic[raw].append(
+                        float(site_table_new[j].strip().split(',')[8]))
+                    raw_sub_peptide_dic[raw].append(
+                        site_table_new[j].strip().split(',')[5])
+            if raw_sub_spectra_dic[raw] == 0:
+                link_site_total_dic[linked_site].append("")
+                link_site_total_dic[linked_site].append("")
+                link_site_total_dic[linked_site].append("")
+            else:
+                link_site_total_dic[linked_site].append(
+                    str(raw_sub_spectra_dic[raw]))
+                link_site_total_dic[linked_site].append(
+                    str(min(raw_sub_E_value_dic[raw])))
+                link_site_total_dic[linked_site].append(
+                    str(len(list(set(raw_sub_peptide_dic[raw])))))
+        b.write(','.join(link_site_total_dic[linked_site]))
+        b.write('\n')
+    b.close()
+    return
 
 
 def statistic_report_file():
     c = open("report.txt", 'a')
     rep_table = open("report.txt").readlines()
-    print(rep_table)
     col_dic = {}
     total_spectra = 0
-    total_colom = len(rep_table[0].strip().split("\t"))
+    total_colom = len(rep_table[0].strip().split(","))
     intra_num = 0
     for i in range(1, len(rep_table)):
-        total_spectra += int(rep_table[i].strip("\n").split("\t")[1])
-        if rep_table[i].strip("\n").split("\t")[5] == "intra":
+        total_spectra += int(rep_table[i].strip("\n").split(",")[1])
+        if rep_table[i].strip("\n").split(",")[5] == "intra":
             intra_num += 1
 
     col_dic[5] = float(intra_num) / (len(rep_table) - 1)
@@ -113,39 +168,41 @@ def statistic_report_file():
     col_dic[2] = ""
     col_dic[3] = ""
     col_dic[4] = ""
+    col_dic[6] = ""
 
     column_sub_dic = {}
-    for k in [6, 8]:
+    for k in [7, 9]:
         for j in range(k, total_colom, 3):
             column_sub_dic[j] = []
             for i in range(1, len(rep_table)):
-                if rep_table[i].strip("\n").split("\t")[j]:
+                if rep_table[i].strip("\n").split(",")[j]:
                     column_sub_dic[j].append(
-                        int(rep_table[i].strip("\n").split("\t")[j]))
+                        int(rep_table[i].strip("\n").split(",")[j]))
             col_dic[j] = sum(column_sub_dic[j])
 
-    for j in range(7, total_colom, 3):
+    for j in range(8, total_colom, 3):
         column_sub_dic[j] = []
         for i in range(1, len(rep_table)):
-            if rep_table[i].strip("\n").split("\t")[j]:
+            if rep_table[i].strip("\n").split(",")[j]:
                 column_sub_dic[j].append(
-                    round(
-                        float(rep_table[i].strip("\n").split("\t")[j]), 2))
+                    round(float(rep_table[i].strip("\n").split(",")[j]), 2))
         col_dic[j] = str(
-            str(min(column_sub_dic[j])) + ',' +
-            str(max(column_sub_dic[j])))
+            str(min(column_sub_dic[j])) + ',' + str(max(column_sub_dic[j])))
     last = []
     for k in range(total_colom):
         last.append(str(col_dic[k]))
-    c.write("\t".join(last))
+    c.write(",".join(last))
     print(last)
     c.close()
+    return
 
 
 def main():
+    remove_sameset()
     get_crosslink_site_info()
     statistic_report_file()
+    os.remove("tmpfile")
 
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     main()
